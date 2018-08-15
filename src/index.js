e93f2b2b41393acd3aeed035ef5527281d9bb359
Steven Ringland <steven.ringland@gmail.com>
2018-08-15 23:55:29 +0100

    init components

--- src/index.js
+++ src/index.js
@@ -7,20 +7,14 @@ import {
 import { A } from "./a";
 import { Alert } from "./alert";
 import { Background } from "./background";
-import { Breadcrumbs } from "./breadcrumbs";
 import { Button } from "./button";
 import { Card, CardHeader, CardContent, CardFooter } from "./card";
 import { Checkbox } from "./checkbox";
-import { Drawer } from "./drawer";
 import { Input } from "./input";
 import { Label } from "./label";
-import { Menu, MenuItem } from "./menu";
-import { Modal } from "./modal";
-import { Pagination } from "./pagination";
 import { Radio } from "./radio";
 import { Select, Option } from "./select";
 import { Textarea } from "./textarea";
-import { Tooltip } from "./tooltip";
 
 export {
   ThemeContext,
@@ -30,24 +24,17 @@ export {
   A,
   Alert,
   Background,
-  Breadcrumbs,
   Button,
   Card,
   CardHeader,
   CardContent,
   CardFooter,
   Checkbox,
-  Drawer,
   Input,
   Label,
-  Menu,
-  MenuItem,
-  Modal,
-  Pagination,
   Radio,
   Select,
   Textarea,
-  Tooltip,
   Option
 };
 
@@ -59,23 +46,16 @@ export default {
   A,
   Alert,
   Background,
-  Breadcrumbs,
   Button,
   Card,
   CardHeader,
   CardContent,
   CardFooter,
   Checkbox,
-  Drawer,
   Input,
   Label,
-  Menu,
-  MenuItem,
-  Modal,
-  Pagination,
   Radio,
   Select,
   Textarea,
-  Tooltip,
   Option
 };
