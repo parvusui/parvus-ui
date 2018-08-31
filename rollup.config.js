f6bd84b24bd4422ef53591c426b7c9c5032a9e49
Steven Ringland <steven.ringland@gmail.com>
2018-08-31 01:18:12 +0100

    added rollup

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify-es';
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'iife',
+      name: 'parvus',
+      file: pkg.main
+    },
+    {
+      format: 'es',
+      file: pkg.module
+    }
+  ],
+  external: [
+    'react', 
+    'react-dom',
+    'create-react-context',
+    'cxs'
+  ],
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    commonJS()
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,34 +5,13 @@ const path = require("path");
 module.exports = {
   target: "web",
   entry: "./examples",
-  output: {
-    path: path.resolve(__dirname, './lib'),
-    filename: "index.js",
-    library: "parvus-ui",
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  resolve: {
-    modules: ["node_modules"]
-  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ["babel-loader", "eslint-loader"]
-      },
-      {
-				test: /\.css$/,
-				loader: 'style-loader!css-loader?importLoaders=1',
-			},
-			{
-				test: /\.svg$/,
-				loader: 'file-loader',
-				query: {
-					name: 'static/media/[name].[hash:8].[ext]',
-				},
-			},
+      }
     ]
   },
   plugins: [new HtmlWebpackPlugin()],
