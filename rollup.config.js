65d7b96872f5158460e366e21b643ca9037c4045
Steven Ringland <steven.ringland@gmail.com>
2018-09-01 15:50:52 +0100

    rollup updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import pkg from './package.json';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
 import commonJS from 'rollup-plugin-commonjs'
 
 export default {
@@ -20,6 +21,7 @@ export default {
     replace({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
+    resolve(),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers']
