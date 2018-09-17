f3a0abadcf13f15f05ab84b6e3026cd0ba5e153c
Steven Ringland <steven.ringland@gmail.com>
2018-09-17 21:32:03 +0100

    menu component added

---
+++ src/menu/Menu.js
@@ -0,0 +1,51 @@
+/* @flow */
+import React, { type Node } from 'react';
+import { withTheme } from '../theme';
+import cxs from 'cxs';
+import { isDarkColor } from '../utils/helpers';
+
+type Props = {
+  children: string | Node,
+  className?: string,
+  styles?: Object,
+  theme?: Object,
+  type: string,
+  otherProps?: Object
+};
+
+const Menu = (props: Props) => {
+  const { className, children, styles, theme, type, ...otherProps } = props;
+
+  const _styles = cxs({
+    backgroundColor: theme ? theme[type + 'Color'] : '#000000',
+    border: '1px solid transparent',
+    borderRadius: theme ? theme.borderRadius : 0,
+    color: theme
+      ? isDarkColor(theme[type + 'Color'])
+        ? '#ffffff'
+        : '#000000'
+      : '#ffffff',
+    fontFamily: theme
+      ? theme.fontFamily
+      : 'Helvetica Neue, Helvetica, Arial, sans-serif',
+    fontSize: '1.25rem',
+    fontWeight: 400,
+    lineHeight: '1.5',
+    textAlign: 'left',
+    width: 'auto',
+    minHeight: '3rem',
+    ...styles
+  });
+
+  return (
+    <div className={`${_styles} '${className || ''}`} {...otherProps}>
+      {children}
+    </div>
+  );
+};
+
+Menu.defaultProps = {
+  type: 'primary'
+};
+
+export default withTheme(Menu);
