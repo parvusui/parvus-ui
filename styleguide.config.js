cd1b106c875a1ff0b3060012ccf215b493cc5636
Steven Ringland <steven.ringland@gmail.com>
2018-09-22 19:47:55 +0100

    styleguide update

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,56 @@
+const path = require('path');
+
 module.exports = {
-	title: 'Parvus-UI Examples',
-	components: 'src/**/[A-Z]*.js'
+	title: 'parvus-ui',
+	components: 'src/**/[A-Z]*.js',
+	theme: {
+		baseBackground: '#fdfdfc',
+		link: '#E2495B',
+		linkHover: '#90a7bf',
+		border: '#e0d2de',
+		font: ['Helvetica', 'sans-serif']
+	},
+	styles: {
+		Playground: {
+			preview: {
+				paddingLeft: 0,
+				paddingRight: 0,
+				borderWidth: [[0, 0, 1, 0]],
+				borderRadius: 0,
+			},
+		},
+		Markdown: {
+			pre: {
+				border: 0,
+				background: 'none',
+			},
+			code: {
+				fontSize: 14,
+			},
+		},
+	},
+	// Override Styleguidist components
+	styleguideComponents: {
+		StyleGuideRenderer: path.join(__dirname, 'styleguide/components/StyleGuide'),
+		SectionsRenderer: path.join(__dirname, 'styleguide/components/SectionsRenderer'),
+	},
+	webpackConfig: {
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+				},
+				{
+					test: /\.css$/,
+					loader: 'style-loader!css-loader?modules',
+				},
+				{
+					test: /\.svg$/,
+					loader: 'url-loader',
+				},
+			],
+		},
+	},
 };
